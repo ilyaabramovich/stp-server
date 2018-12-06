009d3000abc820cc13f8e01ea973649a75ea7fa6
Michael Klimonov <michaelklimonov@MacBook-Pro-Michael.local>
2018-12-06 23:07:47 +0800

    add function of savinf json file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var mysql = require("mysql");
+var fs = require("fs");
 
 var app = express();
 app.use(bodyParser.json());
@@ -12,11 +13,12 @@ app.get("/", function(req, res) {
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  port: '8889',
+  password: "root",
   database: "testso"
 });
 
-app.post("/questions", function(req, res) {
+app.get("/questions", function(req, res) {
   var result = [];
   let str = `SELECT c.name as part, s.name as sections, p.name as subsections, u.name as title,
                 u.difficulty as Complexity, u.hint as description_unit, q.name as text,
@@ -39,7 +41,13 @@ app.post("/questions", function(req, res) {
         });
       });
       console.log(result);
-      res.send(JSON.stringify(result));
+      var answer =  {
+        'questions': result
+      }
+      var json = JSON.stringify(answer, null, 2);
+      json = json.replace('/&/g', '&amp;').replace('/</g', '&lt;').replace('/>/g', '&gt;');
+      fs.writeFileSync('questions.json', json);
+      // res.send();
     }
   );
 });
@@ -48,30 +56,30 @@ app.listen(3000, function() {
   console.log("Example app listening on port 3000!");
 });
 
-var result = [];
-let str = `SELECT c.name as part, s.name as sections, p.name as subsections, u.name as title,
-            u.difficulty as Complexity, u.hint as description_unit, q.name as text,
-            q.hint as description_question, q.typeAnwer as typeAnswer, q.answer as checker
-            FROM chapter c
-            INNER JOIN section s ON s.chapterId=c.id
-            INNER JOIN paragpaph p ON p.sectionId=s.id
-            INNER JOIN unit u ON u.paragraphId=p.id
-            INNER JOIN question q ON q.unitId=u.id
-            WHERE c.name=? AND s.name=?
-            AND p.name=?`;
+// var result = [];
+// let str = `SELECT c.name as part, s.name as sections, p.name as subsections, u.name as title,
+//             u.difficulty as Complexity, u.hint as description_unit, q.name as text,
+//             q.hint as description_question, q.typeAnwer as typeAnswer, q.answer as checker
+//             FROM chapter c
+//             INNER JOIN section s ON s.chapterId=c.id
+//             INNER JOIN paragpaph p ON p.sectionId=s.id
+//             INNER JOIN unit u ON u.paragraphId=p.id
+//             INNER JOIN question q ON q.unitId=u.id
+//             WHERE c.name=? AND s.name=?
+//             AND p.name=?`;
 
-connection.query(
-  str,
-  ["Комбинаторика", "Выборки", "Правило суммы и произведения"],
-  function(err, rows) {
-    if (err) {
-      throw err;
-    }
-    rows.forEach(row => {
-      result.push({
-        ...row
-      });
-    });
-    console.log(result);
-  }
-); //end run
+// connection.query(
+//   str,
+//   ["Комбинаторика", "Выборки", "Правило суммы и произведения"],
+//   function(err, rows) {
+//     if (err) {
+//       throw err;
+//     }
+//     rows.forEach(row => {
+//       result.push({
+//         ...row
+//       });
+//     });
+//     console.log(result);
+//   }
+// ); //end run
